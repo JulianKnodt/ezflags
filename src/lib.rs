0c79832ec374c8ec6c1ed79a09bb7e7108a59805
julianknodt <julianknodt@gmail.com>
2020-03-27 00:30:42 -0700

    Exported Preset as well

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,6 @@
 /// Defines how to make something into a flag,
 /// and how to parse arguments into a flag.
 pub mod flag;
-pub use flag::FlagSet;
+pub use flag::{FlagSet, Preset};
 #[cfg(test)]
 mod tests;
